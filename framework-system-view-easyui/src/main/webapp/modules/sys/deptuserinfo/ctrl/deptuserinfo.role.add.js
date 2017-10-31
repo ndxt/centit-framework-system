e280dc52ca1387ea4830fd7f459406780410d7e1
codefan <codefan@sina.com>
2017-10-31 18:33:12 +0800

    修改一个bug

--- framework-system-view-easyui/src/main/webapp/modules/sys/deptuserinfo/ctrl/deptuserinfo.role.add.js
+++ framework-system-view-easyui/src/main/webapp/modules/sys/deptuserinfo/ctrl/deptuserinfo.role.add.js
@@ -1,10 +1,10 @@
 define(function(require) {
 	var Config = require('config');
 	var Core = require('core/core');
-	
+
 	var Page = require('core/page');
 	var Utils = require('core/utils');
-	
+
 	// 机构添加用户
 	var UserInfoRoleAdd = Page.extend(function() {
 		// @override
@@ -12,31 +12,31 @@ define(function(require) {
 			isPrimary: 'T',
 			obtainDate:Utils.formateDate(new Date(),'yyyy-MM-dd')
 		};
-		
+
 		// @override
 		this.load = function(panel) {
 			this.refresh=false;
 			// 获取父窗口的用户信息
-			var userinfo = this.parent.data;
-			
+			var userinfo = this.parent.data.userInfo;
+
 			var data = this.data = $.extend({}, this.object, {
 				userCode: userinfo.userCode,
 				userName: userinfo.userName
 			});
-			
+
 			panel.form('disableValidation')
 				.find('form')
 				.form('load', data)
 				.form('focus');
 		};
-		
+
 		// @override
 		this.submit = function(panel, data, closeCallback) {
 			var form = panel.find('form');
-			var _self=this;			
+			var _self=this;
 			form.form('enableValidation');
 			var isValid = form.form('validate');
-			
+
 			if (isValid) {
 				form.form('ajax', {
 					url: Config.ContextPath+'system/userrole',
@@ -45,18 +45,18 @@ define(function(require) {
 					_self.refresh=true;
 					closeCallback();
 				});
-				
+
 			}
-			
+
 			return false;
 		};
-		
+
 		// @override
 		this.onClose = function(table) {
 			if(this.refresh)
 			table.datagrid('reload');
 		};
 	});
-	
+
 	return UserInfoRoleAdd;
 });
