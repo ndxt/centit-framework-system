b69021fb901a05fac9d5788e8d278cd0abb02649
wang_xf <wang_xf@centit.com>
2017-11-02 10:24:02 +0800

    备注过长鼠标悬浮显示完整文字

--- framework-system-view-easyui/src/main/webapp/modules/sys/deptrole/ctrl/deptroleinfo.user.js
+++ framework-system-view-easyui/src/main/webapp/modules/sys/deptrole/ctrl/deptroleinfo.user.js
@@ -2,20 +2,20 @@ define(function(require) {
 	var Config = require('config');
 //	var Core = require('core/core');
 	var Page = require('core/page');
-	
+
 	var DeptRoleinfoUserAdd = require('../ctrl/deptroleinfo.user.add');
 //	var DeptRoleinfoUserEdit = require('../ctrl/deptroleinfo.user.edit');
 	var DeptRoleinfoUserRemove = require('../ctrl/deptroleinfo.user.remove');
-	
+
 	var DeptRoleInfoUser = Page.extend(function() {
 		var _self = this;
-		
+
 		this.injecte([
-		  new DeptRoleinfoUserAdd('deptroleinfo_user_add'),   
+		  new DeptRoleinfoUserAdd('deptroleinfo_user_add'),
 //		  new DeptRoleinfoUserEdit('deptroleinfo_user_edit'),
 		  new DeptRoleinfoUserRemove('deptroleinfo_user_remove')
 		]);
-		
+
 		// @override
 		this.load = function(panel, data) {
 			this.data = data;
@@ -24,9 +24,20 @@ define(function(require) {
 			table.cdatagrid({
 				controller: _self,
 				url: Config.ContextPath+'system/userrole/roleusers/'+data.roleCode+'?s_userCode_isValid=T'
-			})
-		};
+			}).datagrid({
+        columns:[[
+          {field:'changeDesc',title:'备注',
+            formatter: function(value,row,index){
+
+              return '<a title="'+value+'">'+value+'</a>';
+
+            }
+          }
+        ]]
+      })
+
+    };
 	});
-	
+
 	return DeptRoleInfoUser;
 });
