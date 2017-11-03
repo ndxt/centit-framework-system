307f8fe75394a3ae3f1ef025739ae24873ae6fba
wang_xf <wang_xf@centit.com>
2017-11-03 10:56:54 +0800

    备注文本过长鼠标悬浮提示

--- framework-system-view-easyui/src/main/webapp/modules/sys/userinfo/ctrl/userinfo.all.js
+++ framework-system-view-easyui/src/main/webapp/modules/sys/userinfo/ctrl/userinfo.all.js
@@ -49,18 +49,15 @@ define(function(require) {
 			var tableRole = panel.find('table.role');
 			tableRole.cdatagrid({
 				controller: _self,
-				url: Config.ContextPath+'system/userrole/userroles/'+data.userCode/*+'?type=S'*/
-			}).datagrid({
-        columns:[[
-          {field:'changeDesc',title:'备注',
-            formatter: function(value,row,index){
-
-              return '<a title="'+value+'">'+value+'</a>';
-
+				url: Config.ContextPath+'system/userrole/userroles/'+data.userCode/*+'?type=S'*/,
+        columns: {
+          changeDesc: {
+            formatter: function (value, row, index) {
+              return '<a title="' + value + '">' + value + '</a>';
             }
           }
-        ]]
-      });
+        }
+			})
 			//获取角色信息table中所需要的数据
 /*			var tableRole = panel.find('table.unitRole');
 			tableRole.cdatagrid({
