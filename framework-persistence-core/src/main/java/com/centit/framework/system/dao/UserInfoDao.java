03d41a263a5c441cc33ac77dca05741fb0ec63e4
codefan <codefan@sina.com>
2017-11-15 08:51:44 +0800

    重构mybatis的接口

--- framework-persistence-core/src/main/java/com/centit/framework/system/dao/UserInfoDao.java
+++ framework-persistence-core/src/main/java/com/centit/framework/system/dao/UserInfoDao.java
@@ -2,7 +2,6 @@ package com.centit.framework.system.dao;
 
 import com.centit.framework.system.po.FVUserOptList;
 import com.centit.framework.system.po.UserInfo;
-
 import java.util.List;
 import java.util.Map;
 
@@ -18,10 +17,9 @@ public interface UserInfoDao {
 
     List<UserInfo> listObjects(Map<String, Object> filterMap);
 
-
     int  pageCount(Map<String, Object> filterDescMap);
-    List<UserInfo>  pageQuery(Map<String, Object> pageQureyMap);
 
+    List<UserInfo>  pageQuery(Map<String, Object> pageQureyMap);
 
     UserInfo getObjectById(String userCode);
 
@@ -30,12 +28,11 @@ public interface UserInfoDao {
      * hql = "SELECT COUNT(*) FROM UserInfo WHERE userCode = " + QueryUtils.buildStringForQuery(user.getUserCode());
      * hql = "SELECT COUNT(*) FROM UserInfo WHERE loginName = " + QueryUtils.buildStringForQuery(user.getLoginName());
      *             " AND userCode  " + QueryUtils.buildStringForQuery(user.getUserCode());
-     * 
      *  放到impl中去了
      * @return int
      */
     //boolean checkIfUserExists(UserInfo user);
-    
+
      int isLoginNameExist(String userCode, String loginName);
      int isCellPhoneExist(String userCode, String regCellPhone);
      int isEmailExist(String userCode, String regEmail);
@@ -47,7 +44,7 @@ public interface UserInfoDao {
 
     //设置主键 o.setUserCode(this.getNextKey()); 和初始密码
     void saveObject(UserInfo o);
-    
+
     //hql = "FROM FVUserOptList urv where urv.id.userCode=?";
     List<FVUserOptList> getAllOptMethodByUser(String userCode);
     /*
@@ -56,40 +53,37 @@ public interface UserInfoDao {
      * "FROM FUserinfo WHERE username = ? AND userpin = ? ", new Object[] {
      * userName, password }).get(0); }
      */
-   
     //return this.listObjectsAll(filterMap);
     List<UserInfo> listUnderUnit(Map<String, Object> filterMap);
 
     // return this.listObjectsAll(filterMap, pageDesc);
     //List<UserInfo> listUnderUnit(Map<String, Object> filterMap, PageDesc pageDesc);
-    
+
     //return getObjectById(userCode);
     UserInfo getUserByCode(String userCode);
-    
-    
+
+
     // return super.getObjectByProperty("loginName",loginName.toLowerCase());
     UserInfo getUserByLoginName(String loginName);
-    
+
     // return super.getObjectByProperty("regEmail", regEmail);
     UserInfo getUserByRegEmail(String regEmail);
-    
+
     //return super.getObjectByProperty("regCellPhone", regCellPhone);
     UserInfo getUserByRegCellPhone(String regCellPhone);
-    
+
     //return super.getObjectByProperty("userTag", userTag);
     UserInfo getUserByTag(String userTag);
-    
+
     //return super.getObjectByProperty("userWord", userWord);
     UserInfo getUserByUserWord(String userWord);
     UserInfo getUserByIdCardNo(String idCardNo);
 
-  
 //    List<UserInfo> listUserinfoByUsercodes(List<String> userCodes);
 
-  
 //    List<UserInfo> listUserinfoByLoginname(List<String> loginnames);
-    
+
     //add by zhuxw
     void restPwd(UserInfo user);
-    
+
 }
--- framework-persistence-mybatis/src/main/java/com/centit/framework/system/dao/mybatisimpl/UserInfoDao.java
+++ framework-persistence-mybatis/src/main/java/com/centit/framework/system/dao/mybatisimpl/UserInfoDao.java
@@ -1,11 +1,43 @@
 package com.centit.framework.system.dao.mybatisimpl;
 
+import com.centit.framework.system.po.FVUserOptList;
+import com.centit.framework.system.po.UserInfo;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("userInfoDao")
 public interface UserInfoDao
     extends com.centit.framework.system.dao.UserInfoDao {
+      @Override
+      List<FVUserOptList> getAllOptMethodByUser(@Param("userCode") String userCode);
+
+      @Override
+      UserInfo getUserByCode(@Param("userCode") String userCode);
+      // return super.getObjectByProperty("loginName",loginName.toLowerCase());
+      @Override
+      UserInfo getUserByLoginName(@Param("loginName") String loginName);
+
+      // return super.getObjectByProperty("regEmail", regEmail);
+      @Override
+      UserInfo getUserByRegEmail(@Param("regEmail") String regEmail);
+
+      //return super.getObjectByProperty("regCellPhone", regCellPhone);
+      @Override
+      UserInfo getUserByRegCellPhone(@Param("regCellPhone") String regCellPhone);
+
+      //return super.getObjectByProperty("userTag", userTag);
+      @Override
+      UserInfo getUserByTag(@Param("userTag") String userTag);
+
+      //return super.getObjectByProperty("userWord", userWord);
+      @Override
+      UserInfo getUserByUserWord(@Param("userWord") String userWord);
+      @Override
+      UserInfo getUserByIdCardNo(@Param("idCardNo") String idCardNo);
+
+
       @Override
       int isLoginNameExist(@Param("userCode") String userCode, @Param("loginName") String loginName);
       @Override
