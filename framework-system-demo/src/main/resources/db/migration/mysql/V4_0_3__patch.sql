7ca768cea6beae3ccb1e7b7be0adae4a54522805
codefan <codefan@sina.com>
2017-11-20 13:36:41 +0800

    添加 , a.UNIT_CODE as inheritedFrom

--- framework-system-demo/src/main/resources/db/migration/mysql/V4_0_3__patch.sql
+++ framework-system-demo/src/main/resources/db/migration/mysql/V4_0_3__patch.sql
@@ -22,11 +22,13 @@ alter table F_UNITROLE
 /* View: F_V_USERROLES                                          */
 /*==============================================================*/
 create or replace view F_V_USERROLES as
-select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'D' as OBTAIN_TYPE, b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,a.USER_CODE
+select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'D' as OBTAIN_TYPE,
+      b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,a.USER_CODE, NULL as inheritedFrom
     from F_USERROLE a join F_ROLEINFO b on (a.ROLE_CODE=b.ROLE_CODE)
     where a.OBTAIN_DATE <=  now() and (a.SECEDE_DATE is null or a.SECEDE_DATE > now()) and b.IS_VALID='T'
 union
-  select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'I' as OBTAIN_TYPE, b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,c.USER_CODE
+  select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'I' as OBTAIN_TYPE,
+        b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,c.USER_CODE, a.UNIT_CODE as inheritedFrom
     from F_UNITROLE a join F_ROLEINFO b on (a.ROLE_CODE = b.ROLE_CODE) JOIN F_USERUNIT c on( a.UNIT_CODE = c.UNIT_CODE)
     where a.OBTAIN_DATE <=  now() and (a.SECEDE_DATE is null or a.SECEDE_DATE > now()) and b.IS_VALID='T'
 
--- framework-system-demo/src/main/resources/db/migration/oracle/V4_0_3__patch.sql
+++ framework-system-demo/src/main/resources/db/migration/oracle/V4_0_3__patch.sql
@@ -15,10 +15,12 @@ create table F_UNITROLE
 alter table F_UNITROLE add primary key (UNIT_CODE, ROLE_CODE);
 
 create or replace view F_V_USERROLES as
-select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'D' as OBTAIN_TYPE, b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,a.USER_CODE
+select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'D' as OBTAIN_TYPE,
+       b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,a.USER_CODE, null as inheritedFrom
     from F_USERROLE a join F_ROLEINFO b on (a.ROLE_CODE=b.ROLE_CODE)
     where a.OBTAIN_DATE <=  sysdate and (a.SECEDE_DATE is null or a.SECEDE_DATE > sysdate) and b.IS_VALID='T'
 union
-  select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'I' as OBTAIN_TYPE, b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,c.USER_CODE
+  select b.ROLE_CODE, b.ROLE_NAME, b.IS_VALID, 'I' as OBTAIN_TYPE,
+      b.ROLE_DESC, b.CREATE_DATE, b.UPDATE_DATE ,c.USER_CODE, a.UNIT_CODE as inheritedFrom
     from F_UNITROLE a join F_ROLEINFO b on (a.ROLE_CODE = b.ROLE_CODE) JOIN F_USERUNIT c on( a.UNIT_CODE = c.UNIT_CODE)
     where a.OBTAIN_DATE <=  sysdate and (a.SECEDE_DATE is null or a.SECEDE_DATE > sysdate) and b.IS_VALID='T'
