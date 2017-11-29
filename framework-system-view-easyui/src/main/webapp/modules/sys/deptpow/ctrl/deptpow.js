bbf67ab85be788a3eae2cb47395adf422a443503
zk <zk@centit.com>
2017-11-29 17:58:05 +0800

    整合机构和部门

--- framework-system-view-easyui/src/main/webapp/modules/sys/deptpow/ctrl/deptpow.js
+++ framework-system-view-easyui/src/main/webapp/modules/sys/deptpow/ctrl/deptpow.js
@@ -1,81 +1,26 @@
-define(function(require) {
-	var Config = require('config');
-	var Core = require('core/core');
-	var Page = require('core/page');
+define(function (require) {
+  var UnitInfo = require('modules/sys/unitinfo/ctrl/unitinfo');
 
-	var UnitInfoAdd = require('../ctrl/deptpow.add');
-	var UnitInfoEdit = require('../ctrl/deptpow.edit');
-	var UnitInfoRemove = require('../ctrl/deptpow.remove');
+  var UnitInfoAdd = require('modules/sys/unitinfo/ctrl/unitinfo.add');
+  var UnitInfoAddTop = require('modules/sys/unitinfo/ctrl/unitinfo.add.top');
+  var UnitInfoEdit = require('modules/sys/unitinfo/ctrl/unitinfo.edit');
+  var UnitInfoRemove = require('modules/sys/unitinfo/ctrl/unitinfo.remove');
+  var UnitInfoAside = require('modules/sys/unitinfo/ctrl/unitinfo.aside');
+  var UnitOperate = require('modules/sys/unitinfo/ctrl/unitinfo.operate');
 
-	var UnitInfoUser = require('../ctrl/deptpow.user');
-	var unitInfoUser = new UnitInfoUser('deptpow_user');
+  return UnitInfo.extend(function () {
 
-	var DeptPow = Page.extend(function() {
+    var UnitInfoAsideImpl = new UnitInfoAside('unitinfo_aside');
 
-		this.injecte([
-		  new UnitInfoAdd('deptpow_add'),
-		  new UnitInfoEdit('deptpow_edit'),
-		  new UnitInfoRemove('deptpow_remove'),
-		  unitInfoUser
-		]);
-		var userpanel = $('#deptpow_panel').layout('panel', 'east');
-		var currentUnit=null;
-		// @override
-		this.load = function(panel) {
-			var table = this.table = panel.find('table');
+    this.injecte([
+      new UnitInfoAddTop('unitinfo_add_top'),
+      new UnitInfoAdd('unitinfo_add'),
+      new UnitInfoEdit('unitinfo_edit'),
+      new UnitInfoRemove('unitinfo_remove'),
+      new UnitOperate('unitinfo_operate'),
+      UnitInfoAsideImpl
+    ]);
 
-			table.ctreegrid({
-				controller: this,
-
-				rowStyler: function(row) {
-					if (row && row.isValid == 'F') {
-						return {
-							'class': 'ban'
-						};
-					}
-				},
-				//给table增加单行点击事件
-				onClickRow: function(row) {
-					// 点击当前节点不响应
-					if (row.unitCode == currentUnit) return;
-					currentUnit = row.unitCode;
-					userpanel.data('panel').options.onLoad = function() {
-						unitInfoUser.init(userpanel, row);
-					};
-					userpanel.panel('setTitle', row.unitName + ' 组织信息');
-					userpanel.panel('refresh', Config.ViewContextPath + 'modules/sys/deptpow/deptpow-user.html');
-				}
-			});
-		};
-
-	});
-
-	return DeptPow;
+    this.queryUrl = 'system/unitinfo/subunits?struct=true';
+  });
 });
-
-function filterData(datas, callback, childField, level, filter) {
-	callback = callback || $.noop;
-	childField = childField || "children";
-	level = level || 0;
-	var recursion = function(data, callback, parent, level) {
-//		data.forEach(function(obj, index) {
-		for (var i = 0; i < data.length; i++) {
-			var obj = data[i];
-			if (filter && obj.unitCode == filter) {
-				parent.children.splice(i, 1);
-				i--;
-			} else {
-				obj["id"] = obj.unitCode;
-				obj["text"] = obj.unitName;
-				var children = obj[childField];
-				if (children && children.length)
-					recursion(children, callback, obj, level + 1);
-				// 当前对象、父级对象、层级、当前对象的index
-				callback.call(obj, obj, parent, level, i);
-			}
-		}
-//		});
-	};
-	datas.length ? recursion(datas, callback, null, 0) : recursion([datas], callback, null, 0);
-	return datas;
-}
