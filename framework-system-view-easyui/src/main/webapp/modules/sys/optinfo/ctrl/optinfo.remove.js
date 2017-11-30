6a9501abf25feb80a5cd26468ea11af0138d5fb3
zk <zk@centit.com>
2017-11-30 13:52:43 +0800

    修改业务菜单布局和remove控制器中的一处错误

--- framework-system-view-easyui/src/main/webapp/modules/sys/optinfo/ctrl/optinfo.remove.js
+++ framework-system-view-easyui/src/main/webapp/modules/sys/optinfo/ctrl/optinfo.remove.js
@@ -1,34 +1,29 @@
-define(function(require) {
-	var Config = require('config');
-	var Core = require('core/core');
-	var Page = require('core/page');
-	
-	// 删除数据字典
-	var OptInfoRemove = Page.extend(function() {
-		
-		// @override
-		this.submit = function(table, data) {
-			Core.ajax(Config.ContextPath+'system/optinfo/'+data.id, {
-            	type: 'json',
-                method: 'post',
-                data: {
-                    _method: 'delete'
-                }
-			}).then(function() {
-            	table.treegrid('remove', data.id);
-            });
-		}
-		this.renderButton=function(target,data){
-			if(data.state=='closed')
-				return false;
-			else{
-				if(data.children.length==0){
-					return true;
-				}
-				return false;
-			}
-		};
-	});
-	
-	return OptInfoRemove;
+define(function (require) {
+  var Config = require('config');
+  var Core = require('core/core');
+  var Page = require('core/page');
+
+  return Page.extend(function () {
+
+    // @override
+    this.submit = function (table, data) {
+      Core.ajax(Config.ContextPath + 'system/optinfo/' + data.id, {
+        type: 'json',
+        method: 'post',
+        data: {
+          _method: 'delete'
+        }
+      }).then(function () {
+        table.treegrid('remove', data.id);
+      });
+    };
+
+    this.renderButton = function (target, data) {
+      if (data.state === 'closed')
+        return false;
+      else {
+        return data.children && data.children.length === 0;
+      }
+    };
+  });
 });
