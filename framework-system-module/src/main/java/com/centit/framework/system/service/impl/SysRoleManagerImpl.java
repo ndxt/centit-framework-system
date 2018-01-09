d2db3860f67a2186196dab6d0d7a7673902bbd2c
zou_wy <zou_wy@centit.com>
2018-01-09 11:09:16 +0800

    删除部门操作权限bug

--- framework-system-module/src/main/java/com/centit/framework/system/service/impl/SysRoleManagerImpl.java
+++ framework-system-module/src/main/java/com/centit/framework/system/service/impl/SysRoleManagerImpl.java
@@ -176,9 +176,9 @@ public class SysRoleManagerImpl implements SysRoleManager {
 
         if( forUpdate.getRight() != null){
             for(RolePower rp : forUpdate.getRight()){
-                rolePowerDao.deleteObject(rp);
+                rolePowerDao.deleteObjectById(new RolePowerId(rp.getRoleCode(), rp.getOptCode()));
                 //如果删除部门操作权限,则要同步删除该部门下角色对应的操作权限
-                /*if(rp.getRoleCode().startsWith("G$")){
+                if(rp.getRoleCode().startsWith("G$")){
                     String optCode = rp.getOptCode();
                     String unitCode = rp.getRoleCode().substring(2);
                     List<RoleInfo> roleInfos = roleInfoDao.listObjects(
@@ -186,7 +186,7 @@ public class SysRoleManagerImpl implements SysRoleManager {
                     for(RoleInfo roleInfo : roleInfos){
                         rolePowerDao.deleteObjectById(new RolePowerId(roleInfo.getRoleCode(), optCode));
                     }
-                }*/
+                }
             }
         }
         if( forUpdate.getLeft() != null){
