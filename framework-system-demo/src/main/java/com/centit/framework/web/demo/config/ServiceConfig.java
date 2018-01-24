68ba8a0bdb93cf9fba014bd01ff82a9684d54018
杨淮生 <codefan@centit.com>
2018-01-24 09:42:17 +0800

    删除 没有必要的文件

--- framework-system-demo/src/main/java/com/centit/framework/web/demo/config/ServiceConfig.java
+++ framework-system-demo/src/main/java/com/centit/framework/web/demo/config/ServiceConfig.java
@@ -4,7 +4,7 @@ import com.centit.framework.components.impl.NotificationCenterImpl;
 import com.centit.framework.components.impl.TextOperationLogWriterImpl;
 import com.centit.framework.config.SpringSecurityCasConfig;
 import com.centit.framework.config.SpringSecurityDaoConfig;
-import com.centit.framework.hibernate.config.HibernateConfig;
+import com.centit.framework.jdbc.config.JdbcConfig;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
 import com.centit.framework.system.config.SystemBeanConfig;
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.*;
  * Created by codefan on 17-7-18.
  */
 @Configuration
-@Import({HibernateConfig.class,
+@Import({JdbcConfig.class,
         SystemBeanConfig.class,
         SpringSecurityDaoConfig.class,
         SpringSecurityCasConfig.class})
