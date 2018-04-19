f540b3ad48f6f52a629cbc4f21e2ce2b93e6cbf2
杨淮生 <codefan@centit.com>
2018-04-19 14:02:53 +0800

    修改bug

--- framework-system-demo/src/main/java/com/centit/framework/web/demo/config/InstantiationServiceBeanPostProcessor.java
+++ framework-system-demo/src/main/java/com/centit/framework/web/demo/config/InstantiationServiceBeanPostProcessor.java
@@ -4,10 +4,12 @@ import com.centit.framework.components.OperationLogCenter;
 import com.centit.framework.model.adapter.MessageSender;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
-import com.centit.framework.system.service.SysRoleManager;
+import com.centit.framework.model.adapter.PlatformEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by codefan on 17-7-6.
@@ -24,17 +26,20 @@ public class InstantiationServiceBeanPostProcessor implements ApplicationListene
     private MessageSender innerMessageManager;
 
     @Autowired
-    private SysRoleManager sysRoleManager;
+    @NotNull
+    private PlatformEnvironment platformEnvironment;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event)
     {
-        sysRoleManager.loadRoleSecurityMetadata();
+        platformEnvironment.reloadSecurityMetadata();
 
-        if(innerMessageManager!=null)
+        if(innerMessageManager!=null) {
             notificationCenter.registerMessageSender("innerMsg", innerMessageManager);
-        if(optLogManager!=null)
+        }
+        if(optLogManager!=null) {
             OperationLogCenter.registerOperationLogWriter(optLogManager);
+        }
     }
 
 }
