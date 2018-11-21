23e325353301a30b4e483ba5d911f72db9f48482
杨淮生 <codefan@centit.com>
2018-11-21 09:43:15 +0800

    @EnableAspectJAutoProxy 注解添加在这个位置才会生效

--- framework-system-demo/src/main/java/com/centit/framework/web/demo/config/NormalSpringMvcConfig.java
+++ framework-system-demo/src/main/java/com/centit/framework/web/demo/config/NormalSpringMvcConfig.java
@@ -2,12 +2,12 @@ package com.centit.framework.web.demo.config;
 
 import com.centit.framework.config.BaseSpringMvcConfig;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * Created by zou_wy on 2017/3/29.
  */
-@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = {"com.otherpackage.controller"},
         includeFilters = {@ComponentScan.Filter(value= org.springframework.stereotype.Controller.class)},
         useDefaultFilters = false)
