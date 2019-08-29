c4f85371ee413b30e19f74f0b4eaf71c94d7bf4c
codefan <codefan@sina.com>
2019-08-29 09:55:11 +0800

    修改id生成策略，添加了uuid的选项

--- framework-system-module/src/main/java/com/centit/framework/system/service/impl/PersistenceUtils.java
+++ framework-system-module/src/main/java/com/centit/framework/system/service/impl/PersistenceUtils.java
@@ -2,32 +2,31 @@ package com.centit.framework.system.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.centit.framework.system.dao.UnitInfoDao;
-import com.centit.framework.system.po.UnitInfo;
 import com.centit.support.algorithm.NumberBaseOpt;
 import com.centit.support.algorithm.StringBaseOpt;
 import com.centit.support.algorithm.UuidOpt;
 import org.apache.commons.lang3.StringUtils;
 
-public abstract class PersistenceUtils {
+abstract class PersistenceUtils {
 
-    public static String makeIdByFormat(String idno, String idFormate,
+    static String makeIdByFormat(String idno, String idFormat,
                                         String defaultPrefix, int defaultLen, String defaultPad){
-        if(StringUtils.equalsIgnoreCase("uuid",idFormate)){
+        if(StringUtils.equalsIgnoreCase("uuid",idFormat)){
             return UuidOpt.getUuidAsString32();
-        } else if(StringUtils.equalsIgnoreCase("uuid22",idFormate)){
+        } else if(StringUtils.equalsIgnoreCase("uuid22",idFormat)){
             return UuidOpt.getUuidAsString22();
         } else {
             //{"prefix":"U","length":8,"pad":"0"}
             String prefix = defaultPrefix;
             String pad = defaultPad;
             int len = defaultLen;
-
-            JSONObject idFormat = (JSONObject) JSON.parse(idFormate);
-            if(idFormat!=null) {
-                len =NumberBaseOpt.castObjectToInteger(idFormat.get("length"), 1);
-                prefix = idFormat.getString("prefix");
-                pad = idFormat.getString("pad");
+            if(StringUtils.isNotBlank(idFormat)) {
+                JSONObject idJsonFormat = (JSONObject) JSON.parse(idFormat);
+                if (idJsonFormat != null) {
+                    len = NumberBaseOpt.castObjectToInteger(idJsonFormat.get("length"), 1);
+                    prefix = idJsonFormat.getString("prefix");
+                    pad = idJsonFormat.getString("pad");
+                }
             }
             return StringBaseOpt.midPad(idno, len, prefix, pad);
         }
