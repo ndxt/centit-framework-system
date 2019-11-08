e8470a4265f082ae5d5368ed431c9a0b7712d380
codefan <codefan@sina.com>
2019-11-08 11:17:00 +0800

    修改 bug

--- framework-system-demo/src/main/java/com/centit/prodcts/demoapp/config/WebInitializer.java
+++ framework-system-demo/src/main/java/com/centit/prodcts/demoapp/config/WebInitializer.java
@@ -3,76 +3,36 @@ package com.centit.prodcts.demoapp.config;
 import com.centit.framework.config.SystemSpringMvcConfig;
 import com.centit.framework.config.WebConfig;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-import javax.servlet.ServletRegistration.Dynamic;
 
 
 /**
  * Created by zou_wy on 2017/3/29.
  */
-
-
 public class WebInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
-        initializeSpringConfig(servletContext);
+        String[] servletUrlPatterns = {"/system/*", "/test/*"};
+        WebConfig.registerSpringConfig(servletContext, ServiceConfig.class);
+        WebConfig.registerServletConfig(servletContext, "system",
+            "/system/*",
+            SystemSpringMvcConfig.class, SwaggerConfig.class);
+        WebConfig.registerServletConfig(servletContext, "test",
+            "/test/*",
+            NormalSpringMvcConfig.class, SwaggerConfig.class);
 
-        initializeSystemSpringMvcConfig(servletContext);
-        initializeNormalSpringMvcConfig(servletContext);
-        String [] servletUrlPatterns = {"/system/*","/test/*"};
-        WebConfig.registerSpringSecurityFilter(servletContext,servletUrlPatterns);
         WebConfig.registerRequestContextListener(servletContext);
         WebConfig.registerSingleSignOutHttpSessionListener(servletContext);
-        //WebConfig.registerResponseCorsFilter(servletContext);
-        WebConfig.registerCharacterEncodingFilter(servletContext,servletUrlPatterns);
-        WebConfig.registerHttpPutFormContentFilter(servletContext,servletUrlPatterns);
-        WebConfig.registerHiddenHttpMethodFilter(servletContext,servletUrlPatterns);
+        WebConfig.registerCharacterEncodingFilter(servletContext, servletUrlPatterns);
+        WebConfig.registerHttpPutFormContentFilter(servletContext, servletUrlPatterns);
+        WebConfig.registerHiddenHttpMethodFilter(servletContext, servletUrlPatterns);
         WebConfig.registerRequestThreadLocalFilter(servletContext);
-
-        WebConfig.initializeH2Console(servletContext);
+        WebConfig.registerSpringSecurityFilter(servletContext, servletUrlPatterns);
     }
 
-    /**
-     * 加载Spring 配置
-     * @param servletContext ServletContext
-     */
-    private void initializeSpringConfig(ServletContext servletContext){
-        AnnotationConfigWebApplicationContext springContext = new AnnotationConfigWebApplicationContext();
-        springContext.register(ServiceConfig.class);
-        servletContext.addListener(new ContextLoaderListener(springContext));
-    }
 
-    /**
-     * 加载Servlet 配置
-     * @param servletContext ServletContext
-     */
-    private void initializeSystemSpringMvcConfig(ServletContext servletContext) {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(SystemSpringMvcConfig.class, SwaggerConfig.class);
-        Dynamic system  = servletContext.addServlet("system", new DispatcherServlet(context));
-        system.addMapping("/system/*");
-        system.setLoadOnStartup(1);
-        system.setAsyncSupported(true);
-    }
-
-    /**
-     * 加载Servlet 项目配置
-     * @param servletContext ServletContext
-     */
-    private void initializeNormalSpringMvcConfig(ServletContext servletContext) {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(NormalSpringMvcConfig.class, SwaggerConfig.class);
-        ServletRegistration.Dynamic system  = servletContext.addServlet("test", new DispatcherServlet(context));
-        system.addMapping("/test/*");
-        system.setLoadOnStartup(1);
-        system.setAsyncSupported(true);
-    }
 }
