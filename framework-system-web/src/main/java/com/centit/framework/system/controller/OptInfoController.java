6a681ac8fcd26a366282ad7485be75b43b5ebb72
codefan <codefan@sina.com>
2019-12-13 16:02:34 +0800

    修改bug

--- framework-system-web/src/main/java/com/centit/framework/system/controller/OptInfoController.java
+++ framework-system-web/src/main/java/com/centit/framework/system/controller/OptInfoController.java
@@ -260,6 +260,7 @@ public class OptInfoController extends BaseController {
         }
 
         for (OptMethod optDef : optInfo.getOptMethods()) {
+            optDef.setOptId(optId);
             if (StringUtils.isBlank(optDef.getOptCode())) {
                 optDef.setOptCode(optMethodManager.getNextOptCode());
             }
