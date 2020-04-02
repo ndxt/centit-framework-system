4f755c15e4f2d3435a78d9c26c6bfdb4ea89df10
codefan <codefan@sina.com>
2020-04-02 16:44:07 +0800

    同步代碼

--- framework-system-demo/src/main/java/com/otherpackage/service/impl/TestCaseManagerImpl.java
+++ framework-system-demo/src/main/java/com/otherpackage/service/impl/TestCaseManagerImpl.java
@@ -3,8 +3,8 @@ package com.otherpackage.service.impl;
 import com.alibaba.fastjson.JSONArray;
 import com.centit.framework.core.dao.DataPowerFilter;
 import com.centit.framework.core.dao.DictionaryMapUtils;
+import com.centit.framework.core.service.DataScopePowerManager;
 import com.centit.framework.security.model.CentitUserDetails;
-import com.centit.framework.system.service.GeneralService;
 import com.centit.support.database.utils.PageDesc;
 import com.otherpackage.dao.TestCaseDao;
 import com.otherpackage.po.TestCase;
@@ -25,7 +25,7 @@ public class TestCaseManagerImpl implements TestCaseManager {
     }
     // 框架通用的服务，可以访问框架的配置信息
     @Autowired
-    protected GeneralService generalService;
+    protected DataScopePowerManager dataScopePowerManager;
     // 业务对应的数据库访问到
     @Autowired
     protected TestCaseDao testCaseDao;
@@ -34,11 +34,11 @@ public class TestCaseManagerImpl implements TestCaseManager {
     public JSONArray listObjectsAsJson(CentitUserDetails ud,
                                 Map<String, Object> filterMap, PageDesc pageDesc){
         // 创建当权用户信息相关的上下文
-        DataPowerFilter dataPowerFilter = generalService.createUserDataPowerFilter(ud);
+        DataPowerFilter dataPowerFilter = dataScopePowerManager.createUserDataPowerFilter(ud);
         // 将当前查询参数（一般是前台输入的参数）添加到这个用户信息中
         dataPowerFilter.addSourceData(filterMap);
         // 获取用户权限范围过滤条件，getOptId() 获取当前业务ID，  query为当前业务操作代码optCode
-        List<String> filters = generalService.listUserDataFiltersByOptIdAndMethod(
+        List<String> filters = dataScopePowerManager.listUserDataFiltersByOptIdAndMethod(
             ud.getUserCode(), getOptId(), "query");
         return DictionaryMapUtils.mapJsonArray( // 数据字典翻译工作
             this.testCaseDao.listObjectsAsJson( // 执行数据驱动查询
@@ -56,9 +56,9 @@ public class TestCaseManagerImpl implements TestCaseManager {
     @Override
     public boolean saveNewCase(CentitUserDetails ud, TestCase testCase){
         // 创建当权用户信息相关的上下文
-        DataPowerFilter dataPowerFilter = generalService.createUserDataPowerFilter(ud);
+        DataPowerFilter dataPowerFilter = dataScopePowerManager.createUserDataPowerFilter(ud);
         // 获取用户权限范围过滤条件，getOptId() 获取当前业务ID，  saveNew 为当前业务操作代码optCode
-        List<String> filters = generalService.listUserDataFiltersByOptIdAndMethod(
+        List<String> filters = dataScopePowerManager.listUserDataFiltersByOptIdAndMethod(
             ud.getUserCode(), getOptId(), "saveNew");
         boolean passed = dataPowerFilter.checkObject(testCase, filters);
         if(passed){
