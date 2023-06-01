f38dcd041663fb464024095e43b3180b46f8c491
codefan <codefan@centit.com>
2023-06-01 11:06:58 +0800

    修改类型转换bug

--- framework-system-module/src/test/java/com/centit/test/TestUnitFilter.java
+++ framework-system-module/src/test/java/com/centit/test/TestUnitFilter.java
@@ -7,7 +7,8 @@ import java.util.Set;
 public class TestUnitFilter {
     public static void main(String arg[]){
 
-        Set<String> ss = SysUnitFilterEngine.calcSystemUnitsByExp("u0001||u0002||u0003",null,null);
+        Set<String> ss = SysUnitFilterEngine.calcSystemUnitsByExp("u0001||u0002||u0003",
+            "system", null,null);
 
         for(String s:ss)
             System.out.println(s);
