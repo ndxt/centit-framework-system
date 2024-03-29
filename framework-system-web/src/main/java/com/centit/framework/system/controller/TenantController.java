255cf8602024e529b757531d32cd65b588e408aa
codefan <codefan@centit.com>
2024-03-29 13:40:19 +0800

    修改 租户归属人没有管理员身份的逻辑bug

--- framework-system-web/src/main/java/com/centit/framework/system/controller/TenantController.java
+++ framework-system-web/src/main/java/com/centit/framework/system/controller/TenantController.java
@@ -679,10 +679,10 @@ public class TenantController extends BaseController {
     @WrapUpResponseBody
     public ResponseData addTenantUser(@ParamName("us") @Valid UserInfo userInfo, UserUnit userUnit, HttpServletRequest request) {
         String userCode = WebOptUtils.getCurrentUserCode(request);
-        String topUnit = WebOptUtils.getCurrentTopUnit(request);
         if (null == userUnit || StringUtils.isBlank(userUnit.getTopUnit())){
             return ResponseData.makeErrorMessage("topUnit不能为空!");
         }
+        String topUnit = WebOptUtils.getCurrentTopUnit(request);
         if(!StringUtils.equals(userUnit.getTopUnit(), topUnit)){
             return ResponseData.makeErrorMessage(403, "只能添加本租户下的用户!");
         }
