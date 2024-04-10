8721f2226e90ec19a8b11652451a70dbbbe2b4e3
codefan <codefan@centit.com>
2024-04-10 11:52:06 +0800

    修改国际化资源文件名称 locodemsgs

--- framework-system-config/src/main/java/com/centit/framework/system/config/SystemBeanConfig.java
+++ framework-system-config/src/main/java/com/centit/framework/system/config/SystemBeanConfig.java
@@ -56,7 +56,7 @@ public class SystemBeanConfig implements EnvironmentAware {
     MessageSource messageSource() {
         ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
         ms.setUseCodeAsDefaultMessage(true);
-        ms.addBasenames("classpath:i18n/messages");
+        ms.addBasenames("classpath:i18n/locodemsgs");
         ms.setDefaultEncoding("UTF-8");
         return ms;
     }
