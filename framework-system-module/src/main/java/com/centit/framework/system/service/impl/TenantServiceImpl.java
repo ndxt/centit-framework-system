ddba97f4ee45b6e1b8d8e86b200bfdb001fdb2ed
codefan <codefan@centit.com>
2024-06-12 10:57:26 +0800

    更改密码有效期

--- framework-system-module/src/main/java/com/centit/framework/system/service/impl/TenantServiceImpl.java
+++ framework-system-module/src/main/java/com/centit/framework/system/service/impl/TenantServiceImpl.java
@@ -37,7 +37,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
