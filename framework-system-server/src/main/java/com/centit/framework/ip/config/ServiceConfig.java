42f428508a36002495461fc083e29ddd47f8da5d
ndxt_yhs <codefan@centit.com>
2024-06-27 09:49:05 +0800

    适配焦点环境

--- framework-system-server/src/main/java/com/centit/framework/ip/config/ServiceConfig.java
+++ framework-system-server/src/main/java/com/centit/framework/ip/config/ServiceConfig.java
@@ -24,6 +24,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -33,6 +34,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
  */
 @Configuration
 @PropertySource("classpath:system.properties")
+@EnableWebSecurity
 @ComponentScan(basePackages = "com.centit",
     excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
 @Import({DubboConfig.class,
