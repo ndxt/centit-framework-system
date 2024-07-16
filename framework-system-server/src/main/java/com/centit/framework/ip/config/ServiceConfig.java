65a89334f27969f12f67227cdffff3fe63dca258
codefan <codefan@centit.com>
2024-07-16 15:19:37 +0800

    更改配置文件

--- framework-system-server/src/main/java/com/centit/framework/ip/config/ServiceConfig.java
+++ framework-system-server/src/main/java/com/centit/framework/ip/config/ServiceConfig.java
@@ -4,7 +4,6 @@ import com.alibaba.nacos.api.annotation.NacosProperties;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import com.centit.framework.components.impl.NotificationCenterImpl;
-import com.centit.framework.config.SpringSecurityCasConfig;
 import com.centit.framework.config.SpringSecurityDaoConfig;
 import com.centit.framework.dubbo.config.DubboConfig;
 import com.centit.framework.jdbc.config.JdbcConfig;
@@ -39,7 +38,6 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
     excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
 @Import({DubboConfig.class,
     SystemBeanConfig.class,
-    SpringSecurityCasConfig.class,
     SpringSecurityDaoConfig.class,
     JdbcConfig.class})
 @EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "${nacos.server-addr}"))
